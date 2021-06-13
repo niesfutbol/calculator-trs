4334e4d422cc7316026a422001228f73294865dd
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-06-12 17:50:07 -0700

    🧪🛑 Add failing test

--- tests/testthat/test_make_fit.R
+++ tests/testthat/test_make_fit.R
@@ -2,7 +2,13 @@ setwd("/workdir")
 library(geci.rjags)
 
 in_file <- "tests/data/statistics_262_2019.csv"
-results <- read.csv(in_file)
-testthat::test_that("ajuste lineal", {
-  testthat::expect_equal(results,results)
+input_file <- read.csv(in_file)
+testthat::test_that("dummy test", {
+  testthat::expect_equal(input_file,input_file)
+})
+
+out_file <- "tests/data/expected_statistics_262_2019.csv"
+output_file <- read.csv(out_file)
+testthat::test_that("obtained expected data", {
+  testthat::expect_equal(input_file, output_file)
 })
