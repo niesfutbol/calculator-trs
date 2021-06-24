b833129a6b7e7b6a454b275dad4b42722b00734f
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-06-23 21:28:07 -0700

    ♻️ Refactor function

--- src/calculate_xGoal.R
+++ src/calculate_xGoal.R
@@ -1,4 +1,4 @@
-library("tidyverse")
+library(tidyverse)
 league <- read_csv("tests/data/statistics_262_2019.csv")
 league_home <- league %>%
   select("home", "home_shots_outsidebox", "home_shots_insidebox") %>%
@@ -9,7 +9,14 @@ league_away <- league %>%
 colnames(league_away) <- c("gol", "outsidebox", "insidebox", "is_home")
 colnames(league_home) <- c("gol", "outsidebox", "insidebox", "is_home")
 cleaned_league <- bind_rows(league_home, league_away)
-modelo <- glm(gol ~ 0 + outsidebox + insidebox, data = cleaned_league, poisson(link = "identity"))
+fit_xGoal <- function(league){
+  modelo <- glm(
+    gol ~ 0 + outsidebox + insidebox,
+    data = league,
+    poisson(link = "sqrt")
+    )
+}
+modelo <- fit_xGoal(cleaned_league)
 resumen_modelo <- summary(modelo)
-xGol_inside <- al$coefficients[2,1]
-xGol_outside <- al$coefficients[1,1]
+xGol_inside <- resumen_modelo$coefficients[2,1]
+xGol_outside <- resumen_modelo$coefficients[1,1]
