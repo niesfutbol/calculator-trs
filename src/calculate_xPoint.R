2c7e0b8a94cdf5a5a127a3e7bf8ad3c86951fd62
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-08-04 21:57:37 -0700

    ✨ Calculate the expected table for the second round

--- src/calculate_xPoint.R
+++ src/calculate_xPoint.R
@@ -1,8 +1,8 @@
 library(tidyverse)
 library(comprehenr)
 calculate_xgoal <- function(shots_outsidebox, shots_insidebox){
-  xGol_inside <- 0.09720
-  xGol_outside <- 0.06995
+  xGol_inside <- 0.101
+  xGol_outside <- 0.043
   xgoal <- shots_outsidebox * xGol_outside + shots_insidebox * xGol_inside
   return(xgoal)
 }
@@ -19,6 +19,12 @@ calculate_xpoints <- function(home_xGol, away_xGol){
   return(xpoints)
 }
 
+calculate_points <- function(home_xGol, away_xGol){
+  diff_goals <- home_xGol - away_xGol
+  points <- sum(diff_goals > 0)*3 + sum(diff_goals == 0)
+  return(points)
+}
+
 add_home_shots_outsidebox <- function(datos){
   salida <- datos %>% mutate("home_shots_outsidebox" = home_total_shots - home_shots_insidebox)
   return(salida)
@@ -29,7 +35,7 @@ add_away_shots_outsidebox <- function(datos){
   return(salida)
 }
 
-league <- read_csv("tests/data/statistics_4_2020.csv")
+league <- read_csv("tests/data/statistics_262_2021.csv")
 league[is.na(league)] <- 0
 league <- league %>%
   add_home_shots_outsidebox() %>%
@@ -42,14 +48,16 @@ league <- league %>%
 number_of_matches <- nrow(league)
 home_xPoints <- to_vec(for(x in 1:number_of_matches) calculate_xpoints(league[x,]$home_xGol, league[x,]$away_xGol))
 away_xPoints <- to_vec(for(x in 1:number_of_matches) calculate_xpoints(league[x,]$away_xGol, league[x,]$home_xGol))
-league <- cbind(league, tibble(home_xPoints, away_xPoints))
+home_Points <- to_vec(for(x in 1:number_of_matches) calculate_points(league[x,]$home, league[x,]$away))
+away_Points <- to_vec(for(x in 1:number_of_matches) calculate_points(league[x,]$away, league[x,]$home))
+league <- cbind(league, tibble(home_xPoints, away_xPoints, home_Points, away_Points))
 
 suspicious_game <- league %>%
   filter(home_alpha < 0.05 | away_alpha < 0.05) %>%
   select(c("match_id", "home", "away", "home_xGol", "away_xGol", "home_xPoints", "away_xPoints"))
 
-resumen <- league %>% 
-  select(home_xPoints, away_xPoints, match_id, home_id, away_id) %>% 
+resumen <- league %>%
+  select(home_xPoints, away_xPoints, match_id, home_id, away_id) %>%
   unite(col="home", c(home_xPoints, home_id), sep="--") %>%
   unite(col="away", c(away_xPoints, away_id), sep="--") %>%
   gather(key="local", value="xPoint-d", -match_id) %>%
@@ -57,6 +65,23 @@ resumen <- league %>%
   group_by(id) %>%
   mutate(xPoints = as.numeric(xPoints)) %>%
   summarize(
-    puntos = sum(xPoints), 
+    xPuntos = sum(xPoints),
+    jj = n()) %>%
+  arrange(desc(xPuntos))
+
+puntos <- league %>%
+  select(home_Points, away_Points, match_id, home_id, away_id) %>%
+  unite(col="home", c(home_Points, home_id), sep="--") %>%
+  unite(col="away", c(away_Points, away_id), sep="--") %>%
+  gather(key="local", value="Point-d", -match_id) %>%
+  separate(col="Point-d", into=c('Points', 'id'), sep='--') %>%
+  group_by(id) %>%
+  mutate(Points = as.numeric(Points)) %>%
+  summarize(
+    puntos = sum(Points),
     jj = n()) %>%
   arrange(desc(puntos))
+
+xTable <- resumen %>% inner_join(puntos)
+xTable$xPuntos <- round(xTable$xPuntos,2)
+xTable %>% write_csv("results/xTable_262_2021.csv")
