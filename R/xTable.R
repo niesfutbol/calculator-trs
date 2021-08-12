0de12b25dc7fe57206fcd5aa8ff856ecf143df58
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-08-11 19:02:58 -0700

    ♻️ Refactor function calculate_points

--- R/xTable.R
+++ R/xTable.R
@@ -1,3 +1,9 @@
 return_one <- function() {
   return(1)
 }
+
+calculate_points <- function(home_xGol, away_xGol){
+  diff_goals <- home_xGol - away_xGol
+  points <- sum(diff_goals > 0)*3 + sum(diff_goals == 0)
+  return(points)
+}
--- src/calculate_xPoint.R
+++ src/calculate_xPoint.R
@@ -19,12 +19,6 @@ calculate_xpoints <- function(home_xGol, away_xGol){
   return(xpoints)
 }
 
-calculate_points <- function(home_xGol, away_xGol){
-  diff_goals <- home_xGol - away_xGol
-  points <- sum(diff_goals > 0)*3 + sum(diff_goals == 0)
-  return(points)
-}
-
 add_home_shots_outsidebox <- function(datos){
   salida <- datos %>% mutate("home_shots_outsidebox" = home_total_shots - home_shots_insidebox)
   return(salida)
