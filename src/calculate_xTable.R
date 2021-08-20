a1b3d8fa94fa63e8084929cb7b1a74575f5c5517
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-08-19 20:39:29 -0700

    ⚡♻️Use refactorized functions

--- src/calculate_xTable.R
+++ src/calculate_xTable.R
@@ -12,31 +12,14 @@ statistics_path <- glue::glue("results/league_{league_season}.csv")
 league <- read_csv(statistics_path)
 
 resumen <- league %>%
-  select(home_xPoints, away_xPoints, home_id, away_id) %>%
-  unite(col="home", c(home_xPoints, home_id), sep="--") %>%
-  unite(col="away", c(away_xPoints, away_id), sep="--") %>%
-  gather(key="local", value="xPoint-d") %>%
-  separate(col="xPoint-d", into=c('xPoints', 'id'), sep='--') %>%
-  group_by(id) %>%
-  mutate(xPoints = as.numeric(xPoints)) %>%
-  summarize(
-    xPuntos = sum(xPoints),
-    jj = n()) %>%
-  arrange(desc(xPuntos))
+  xpoint_team_place() %>%
+  summarize_xpoints_played_match() %>%
+  arrange(desc(xpuntos))
 
 puntos <- league %>%
-  select(home_Points, away_Points, home_id, away_id) %>%
-  unite(col="home", c(home_Points, home_id), sep="--") %>%
-  unite(col="away", c(away_Points, away_id), sep="--") %>%
-  gather(key="local", value="Point-d") %>%
-  separate(col="Point-d", into=c('Points', 'id'), sep='--') %>%
-  group_by(id) %>%
-  mutate(Points = as.numeric(Points)) %>%
-  summarize(
-    puntos = sum(Points),
-    jj = n()) %>%
-  arrange(desc(puntos))
+  point_team_place() %>%
+  summarize_points_played_match()
 
 xTable <- resumen %>% inner_join(puntos)
-xTable$xPuntos <- round(xTable$xPuntos,2)
+xTable$xpuntos <- round(xTable$xpuntos,2)
 xTable %>% write_csv(glue::glue("results/xTable_{league_season}.csv"))
