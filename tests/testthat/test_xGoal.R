7b5c55fe1049b639bc61e5e8b4df5d6765adc6f8
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-08-20 18:54:40 -0700

    🧪🛑 Change to xGol of all the match

--- tests/testthat/test_xGoal.R
+++ tests/testthat/test_xGoal.R
@@ -14,7 +14,8 @@ describe("The class Teams", {
   it("Read a league file", {
     path_league <- "../data/league_262_2021.csv"
     teams$read(path_league)
-    expected_league <- read_csv(path_league)
+    raw_league <- read_csv(path_league)
+    expected_league <- xgoal_team_place(raw_league)
     obtained_league <- teams$league
     expect_equal(expected_league, obtained_league)
   })
