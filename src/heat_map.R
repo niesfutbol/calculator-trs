167cdce713522718bd2a903c7ccd481a60db8b20
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-08-20 22:11:57 -0700

    👩🏿‍💻 Use the class Teams

--- src/heat_map.R
+++ src/heat_map.R
@@ -1,3 +1,12 @@
-away_prob <- (dpois(0:6, league$away_xGol[1]))
-home_prob <- (dpois(0:6, league$home_xGol[1]))
-for (i in 1:5) {print(home_prob[i] * away_prob[1:7])}
+library(tidyverse)
+library(xGoal)
+source("R/xGoal.R")
+#away_prob <- (dpois(0:6, league$away_xGol[1]))
+#home_prob <- (dpois(0:6, league$home_xGol[1]))
+#for (i in 1:5) {print(home_prob[i] * away_prob[1:7])}
+statistics_path <- "results/league_262_2021.csv"
+league <- read_csv(statistics_path)
+
+teams <- Teams$new()
+teams$read(statistics_path)
+teams$get_id_teams()
