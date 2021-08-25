0e94f60732c40d01cf83ea3dae36942d64ad8160
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-08-24 17:45:26 -0700

    🛑🧪 Test the function probability_win_draw_win

--- tests/testthat/test_xGoal.R
+++ tests/testthat/test_xGoal.R
@@ -104,4 +104,11 @@ describe("The function probability_win_draw_win", {
     obtained_probability <- probability_win_draw_win(probable_score)
     expect_equal(expected_probability, obtained_probability)
   })
+  it("Matrix 3x3", {
+    probable_score <- matrix(rep(1/6, 9), nrow = 3)
+    diag(probable_score) <- 0
+    expected_probability <- c(1/2, 0, 1/2)
+    obtained_probability <- probability_win_draw_win(probable_score)
+    expect_equal(expected_probability, obtained_probability)
+  })
 })
