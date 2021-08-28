107960aa5edfedc54004312c8fbbd25c1d67902e
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-08-27 17:44:33 -0700

    🎨 Make format with styler

--- R/xGoal.R
+++ R/xGoal.R
@@ -87,14 +87,14 @@ Heat_Map <- R6::R6Class("Heat_Map",
         scale_fill_gradient(low = "white", high = "red")
       home_prob <- tibble(home = as.character(seq(0, 5)), prob = private$home_probability_goal)
       private$home_barplot <- ggplot(data = home_prob, aes(x = home, y = prob)) +
-        geom_bar(stat = "identity", fill="#EF9A9A") +
+        geom_bar(stat = "identity", fill = "#EF9A9A") +
         lims(y = c(0, 1)) +
         labs(x = "", y = "") +
         theme_classic() +
         ggtitle(self$home_team)
       away_prob <- tibble(away = as.character(seq(0, 5)), prob = private$away_probability_goal)
       private$away_barplot <- ggplot(data = away_prob, aes(x = away, y = prob)) +
-        geom_bar(stat = "identity", fill="#FF8A65") +
+        geom_bar(stat = "identity", fill = "#FF8A65") +
         rotate() +
         scale_y_reverse() +
         lims(y = c(1, 0)) +
