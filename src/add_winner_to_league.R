d1c7d72cb150dee141404c9f489f740d4ccda50a
Nepo Rojas <braulio.rojas@islas.org.mx>
2022-08-05 20:37:30 -0700

    ⚡️ Use join functions

--- src/add_winner_to_league.R
+++ src/add_winner_to_league.R
@@ -4,9 +4,9 @@ source("R/xTable.R")
 opciones <- cli_calculate_xpoints();
 league_season <- opciones[["league-season"]]
 path_names <- glue::glue("tests/data/names_{league_season}.csv")
-names <- read_csv(path_names)
+names <- read_csv(path_names, show_col_types = FALSE)
 path_league <- glue::glue("results/league_{league_season}.csv")
-league <- read_csv(path_league)
+league <- read_csv(path_league, show_col_types = FALSE)
 
 how_won <- function(home, away) {
   if (home > away) {
@@ -23,19 +23,13 @@ how_won <- function(home, away) {
 attack <- comprehenr::to_vec(for (id in names[["ids"]]) get_strength_atack(league, id))
 deffense <- comprehenr::to_vec(for (id in names[["ids"]]) get_strength_deffense(league, id))
 strength <- tibble(ids = names[["ids"]], attack = attack, deffense = deffense)
-add_attack <- function(id) {
-  return(strength %>% filter(ids == id) %>% .$attack)
-}
-add_deffense <- function(id) {
-  return(strength %>% filter(ids == id) %>% .$deffense)
-}
 
 data <- league %>%
   mutate(won = mapply(function(x, y) how_won(x, y), home, away)) %>%
-  mutate(home_attack = mapply(function(x) add_attack(x), home_id)) %>%
-  mutate(away_attack = mapply(function(x) add_attack(x), away_id)) %>%
-  mutate(home_deffense = mapply(function(x) add_deffense(x), home_id)) %>%
-  mutate(away_deffense = mapply(function(x) add_deffense(x), away_id)) %>%
+  left_join(strength, by = c("home_id" = "ids")) %>%
+  rename(home_attack = attack, home_deffense = deffense) %>%
+  left_join(strength, by = c("away_id" = "ids")) %>%
+  rename(away_attack = attack, away_deffense = deffense) %>%
   select(home, away, won, home_attack, home_deffense, away_attack, away_deffense)
 
 path_output <- glue::glue("results/strength_league_{league_season}.csv")
