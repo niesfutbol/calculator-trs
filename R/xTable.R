d43e927fb2a54f786033a47c10f0d6f6f1746f52
Nepo Rojas <braulio.rojas@islas.org.mx>
2022-12-30 13:23:45 -0800

    ✍🏿 Update cli's help

--- R/xTable.R
+++ R/xTable.R
@@ -69,9 +69,7 @@ cli_calculate_xpoints <- function() {
     make_option(
       c("-d", "--directory"),
       default = "tests/data",
-      help = "League and season like 78_2020: \n
-        Bundesliga id is 78 \n
-        Premier id is 39 \n",
+      help = "Directory where are the files `statistics_{league}_{season}.csv`",
       metavar = "character",
       type = "character"
     )
