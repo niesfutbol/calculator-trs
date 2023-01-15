f37bca940bbbdbb44e6c386c6e608391702e3521
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-01-15 14:49:08 -0800

    ♻️✅ Extract function

--- tests/testthat/test-clean_nies_to_drive.R
+++ tests/testthat/test-clean_nies_to_drive.R
@@ -1,10 +1,15 @@
-test_that("We can run the original file", {
+run_clean_nies_to_drive <- function() {
   input_file  <- "/workdir/tests/data/nies_13-01-2022.csv"
   output_file <- "/workdir/tests/data/cleaned_nies.csv"
   src_file    <- "/workdir/src/clean_nies_to_drive.R"
   testtools::delete_output_file(output_file)
   command <- glue::glue("Rscript {src_file} -i {input_file} -o {output_file}")
   system(command)
+}
+
+test_that("We can run the original file", {
+  output_file <- "/workdir/tests/data/cleaned_nies.csv"
+  run_clean_nies_to_drive()
   expected_hash <- "a5bce86a571db25679065a7fc58336ac"
   obtained_hash <- as.vector(tools::md5sum(output_file))
   expect_equal(obtained_hash, expected_hash)
