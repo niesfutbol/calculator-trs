6973a789ac377536f917ab788c20f60c5a99bb1b
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-01-15 18:38:37 -0800

    🛑🧪 Test `calculate_nies_fee()`

--- tests/testthat/test-clean_nies_to_drive.R
+++ tests/testthat/test-clean_nies_to_drive.R
@@ -23,6 +23,7 @@ data_to_test <- tibble(
   draw = c(0.3, 0.1, 0.6),
   away = c(0.1, 0.6, 0.3)
 )
+
 test_that("Mutate who did win", {
   expected <- tibble(
     home_team = "home_team",
@@ -46,5 +47,18 @@ test_that("What's the name of the one who won?", {
     name = c("home_team", "away_team", "Draw")
   )
   obtained <- what_name_of_who_won(data_to_test)
+  expect_equal(obtained, expected)
+})
+
+test_that("Calculate the nies fee", {
+  expected <- tibble(
+    home_team = "home_team",
+    away_team = "away_team",
+    home = c(0.6, 0.3, 0.1),
+    draw = c(0.3, 0.1, 0.6),
+    away = c(0.1, 0.6, 0.3),
+    nies_cuota = 1/0.6
+  )
+  obtained <- calculate_nies_fee(data_to_test)
   expect_equal(obtained, expected)
 })
