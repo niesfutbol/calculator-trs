5a3cadfd23d6034058a36455e9849c49d6a1e25f
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-01-16 20:35:59 -0800

    ☂️ Update coverage

--- coverage.R
+++ coverage.R
@@ -2,10 +2,12 @@ library(covr)
 library(testthat)
 cobertura <- file_coverage(
     c(
+        "R/clean_nies_to_drive.R",
         "R/xGoal.R",
         "R/xTable.R"
     ),
     c(
+        "tests/testthat/test-clean_nies_to_drive.R",
         "tests/testthat/test_xGoal.R",
         "tests/testthat/test_xTable.R"
     )
