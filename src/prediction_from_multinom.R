fb768055cac63325b11bb66da759974941a02089
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-01-21 00:30:56 -0800

    ♻️ Use function `calculate_attack_strength_for_each_team()` and `calculate_defense_strength_for_each_team()`

--- src/prediction_from_multinom.R
+++ src/prediction_from_multinom.R
@@ -38,9 +38,9 @@ round <- season %>%
 home_id <- round$home_id
 away_id <- round$away_id
 home_attack <- calculate_attack_strength_for_each_team(home_id, league)
-home_defense <- comprehenr::to_vec(for (id in home_id) get_strength_defense(league, id))
-away_attack <- comprehenr::to_vec(for (id in away_id) get_strength_atack(league, id))
-away_defense <- comprehenr::to_vec(for (id in away_id) get_strength_defense(league, id))
+home_defense <- calculate_defense_strength_for_each_team(home_id, league)
+away_attack <- calculate_attack_strength_for_each_team(away_id, league)
+away_defense <- calculate_defense_strength_for_each_team(away_id, league)
 to_predict <- tibble(away_attack, away_defense, home_defense, home_attack)
 pred <- predict(model, to_predict, type = "prob")
 (predictions <- tibble("home" = pred[, 3], "draw" = pred[, 2], "away" = pred[, 1]) %>%
