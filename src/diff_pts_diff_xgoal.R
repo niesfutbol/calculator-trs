9cf80b209567f5f439ef30aaaa903149dcb60a7f
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-01-25 19:56:02 -0800

    ✅ Test image hash

--- src/diff_pts_diff_xgoal.R
+++ src/diff_pts_diff_xgoal.R
@@ -1,4 +1,46 @@
+library("ggplot2")
+library("RcppRoll")
 library("tidyverse")
 source("/workdir/R/xTable.R")
 
-league <- read_csv("/workdir/tests/data/league_78_2022.csv", show_col_types = FALSE)
+opciones <- cli_calculate_xpoints()
+league_season <- opciones[["league-season"]]
+directory <- opciones[["directory"]]
+league_path <- glue::glue("{directory}/league_{league_season}.csv")
+league <- read_csv(league_path, show_col_types = FALSE)
+path_names <- glue::glue("{directory}/names_{league_season}.csv")
+names <- read_csv(path_names, show_col_types = FALSE)
+path_season <- glue::glue("{directory}/season_{league_season}.csv")
+season <- read_csv(path_season, show_col_types = FALSE, col_types = "cc") %>%
+  select(c(id_match, date))
+
+league <- league %>% left_join(season, by = c("match_id" = "id_match"))
+
+id_team <- 505
+nombre <- names %>% filter(ids ==id_team) %>% .$names
+print(nombre)
+point <- extract_point_from_league(league, id_team)
+xpoint <- extract_xpoint_from_league(league, id_team)
+date <- extract_date_from_league(league, id_team)
+
+output <- glue::glue("borrame_{id_team}.csv")
+puntos <- tibble(date, xpoint, point) %>% arrange(date)
+puntos$point_6 <- roll_mean(puntos$point, n = 4, align = "right", fill = NA)
+puntos$xpoint_6 <- roll_mean(puntos$xpoint, n = 4, align = "right", fill = NA)
+puntos <- puntos %>% mutate(diff_points = point_6 - xpoint_6)
+puntos %>% write_csv(output)
+
+media <- mean(puntos$diff_points, na.rm = TRUE)
+sd <- sd(puntos$diff_points, na.rm = TRUE)
+print(media)
+
+p <- ggplot(puntos, aes(x=date, y=diff_points)) +
+  geom_line( color="steelblue") +
+  geom_point() +
+  xlab("") +
+  geom_hline(yintercept=media, linetype="dashed", color = "black") +
+  geom_hline(yintercept=media + sd, linetype="dashed", color = "green") +
+  geom_hline(yintercept=media - sd, linetype="dashed", color = "red") +
+  labs(title = nombre, subtitle = league_season)
+output <- glue::glue("{directory}/{nombre}_{league_season}.jpg")
+ggsave(output)
--- tests/testthat/test-diff_pts_diff_xgoal.R
+++ tests/testthat/test-diff_pts_diff_xgoal.R
@@ -1,11 +1,18 @@
-run_diff_pts_diff_xgoal <- function(output_file, mode) {
+run_diff_pts_diff_xgoal <- function() {
+  league_season <- "135_2022"
+  directory <- "/workdir/tests/data"
   src_file <- "/workdir/src/diff_pts_diff_xgoal.R"
-  command <- glue::glue("Rscript {src_file}")
+  command <- glue::glue("Rscript {src_file} -l {league_season} -d {directory}")
   system(command)
 }
 
 describe("Run diff_pts_diff_xgoal", {
   it("run", {
+    output_file <- "/workdir/tests/data/Inter_135_2022.jpg"
+    testtools::delete_output_file(output_file)
     run_diff_pts_diff_xgoal()
+    expected_hash <- "d83f8f78939c833d7828e1fbb16eaa8a"
+    obtained_hash <- as.vector(tools::md5sum(output_file))
+    expect_equal(obtained_hash, expected_hash)
   })
 })
