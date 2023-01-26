4057fe5e545f35409082cb5be76740c1af7a6c82
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-01-25 19:56:37 -0800

    🚧 Add cli

--- src/calculate_xTable.R
+++ src/calculate_xTable.R
@@ -8,8 +8,9 @@ source("R/xTable.R")
 
 opciones <- cli_calculate_xpoints()
 league_season <- opciones[["league-season"]]
-statistics_path <- glue::glue("results/league_{league_season}.csv")
-league <- read_csv(statistics_path)
+directory <- opciones[["directory"]]
+statistics_path <- glue::glue("{directory}/league_{league_season}.csv")
+league <- read_csv(statistics_path, show_col_types = FALSE)
 
 resumen <- league %>%
   xpoint_team_place() %>%
@@ -20,6 +21,10 @@ puntos <- league %>%
   point_team_place() %>%
   summarize_points_played_match()
 
-xTable <- resumen %>% inner_join(puntos)
+xTable <- resumen %>% inner_join(puntos, by = c("team_id", "jj"))
+path_names <- glue::glue("{directory}/names_{league_season}.csv")
+names <- read_csv(path_names, show_col_types = FALSE, col_types = "cc")
 xTable$xpuntos <- round(xTable$xpuntos, 2)
-xTable %>% write_csv(glue::glue("results/xTable_{league_season}.csv"))
+xTable %>%
+  left_join(names, by = c("team_id" = "ids")) %>%
+  write_csv(glue::glue("results/xTable_{league_season}.csv"))
