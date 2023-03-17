7581795fd1470614e3303980b8ad388516e38c72
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-03-17 14:31:28 -0700

    ✅ Pass test

--- R/concatenate_prediction.R
+++ R/concatenate_prediction.R
@@ -7,3 +7,9 @@ obtain_the_last_round_file <- function(predictions_files) {
   id_of_last_round <- which(rounds == max(rounds))
   return(predictions_files[id_of_last_round])
 }
+
+obtain_prediction_of_each_league <- function(league, path = "/workdir/results") {
+  predictions_files <- list.files(path, pattern = glue::glue("^predictions_{league}"))
+  full_path <- paste(path, obtain_the_last_round_file(predictions_files), sep = "/")
+  return(full_path)
+}
