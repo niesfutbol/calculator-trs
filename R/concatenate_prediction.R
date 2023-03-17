c9fa919334dd1bc6950b75564efc3fa4af20ade0
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-03-17 13:05:34 -0700

    🚧 Add first version of `obtain_the_last_round_file()`

---
+++ R/concatenate_prediction.R
@@ -0,0 +1,6 @@
+obtain_the_last_round_file <- function(predictions_files) {
+  files <- str_split(predictions_files, "_")
+  rounds <- to_vec(for (file in files) files[4])
+  id_of_last_round <- which(rounds == max(rounds))
+  return(predictions_files[id_of_last_round])
+}
