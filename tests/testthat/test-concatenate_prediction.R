dbd2ad4226b1c64fbc1bb5b5890f400d4d68eb16
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-03-17 13:19:12 -0700

    🪲✅ Fix test: Add path of develop code

--- tests/testthat/test-concatenate_prediction.R
+++ tests/testthat/test-concatenate_prediction.R
@@ -1,3 +1,5 @@
+source("/workdir/R/concatenate_prediction.R")
+
 describe("obtain_the_last_round_file", {
   it("returns 34", {
     files <- c("predictions_78_2022_16.csv", "predictions_78_2022_14.csv", "predictions_78_2022_34.csv")
