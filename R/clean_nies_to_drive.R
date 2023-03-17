ff756748ef587a569b0cc1ca141be19aa85dbb0d
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-03-17 12:25:53 -0700

    🔥 Remove old code

--- R/clean_nies_to_drive.R
+++ R/clean_nies_to_drive.R
@@ -21,28 +21,6 @@ calculate_nies_fee <- function(filtered_predictions) {
     mutate(nies_cuota = ifelse(home > 0.5, 1 / home, ifelse(away > 0.5, 1 / away, 1 / draw)))
 }
 
-cli_clean_nies_to_drive <- function() {
-  listaOpciones <- list(
-    make_option(
-      c("-i", "--nies-prediction"),
-      default = "/workdir/results/nies_13-01-2022.csv",
-      help = "Input file path of csv with predictions of the week",
-      metavar = "character",
-      type = "character"
-    ),
-    make_option(
-      c("-o", "--cleaned-nies"),
-      default = "/workdir/results/cleaned_nies.csv",
-      help = "Output file path of csv with cleaned predictions of the week",
-      metavar = "character",
-      type = "character"
-    )
-  )
-  opt_parser <- OptionParser(option_list = listaOpciones)
-  opciones <- parse_args(opt_parser)
-  return(opciones)
-}
-
 read_predictions_of_week <- function(options) {
   expected_option <- "nies-predictions"
   rlang::arg_match0(expected_option, names(opciones))
