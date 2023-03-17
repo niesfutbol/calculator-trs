3d1ca4b2fac200bedf4836299bed77ef221ce298
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-03-17 13:19:37 -0700

    ✅ Pass test

--- R/concatenate_prediction.R
+++ R/concatenate_prediction.R
@@ -1,6 +1,9 @@
+library(comprehenr)
+library(tidyverse)
+
 obtain_the_last_round_file <- function(predictions_files) {
   files <- str_split(predictions_files, "_")
-  rounds <- to_vec(for (file in files) files[4])
+  rounds <- to_vec(for (file in files) file[4])
   id_of_last_round <- which(rounds == max(rounds))
   return(predictions_files[id_of_last_round])
 }
