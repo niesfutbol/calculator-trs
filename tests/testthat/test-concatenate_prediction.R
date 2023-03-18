a48c03a1a4ecac14d7693f0faaa6cde34e57a5e7
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-03-18 10:27:19 -0700

    🛑🧪 Test `obtains_the_last_predictions_of_the_all_leagues()`

--- tests/testthat/test-concatenate_prediction.R
+++ tests/testthat/test-concatenate_prediction.R
@@ -1,6 +1,15 @@
 source("/workdir/R/concatenate_prediction.R")
 
 describe("obtain_the_last_round_file", {
+  it("obtains the last predictions of the all leagues", {
+    expected <- c(
+      "/workdir/tests/data/predictions_78_2022_22.csv",
+      "/workdir/tests/data/predictions_88_2022_22.csv",
+      "/workdir/tests/data/predictions_94_2022_18.csv"
+    )
+    obtained <- obtains_the_last_predictions_of_the_all_leagues(root_path = "/workdir/tests/data")
+    expect_equal(obtained, expected)
+  })
   it("returns 34", {
     files <- c("predictions_78_2022_16.csv", "predictions_78_2022_14.csv", "predictions_78_2022_34.csv")
     expected <- "predictions_78_2022_34.csv"
