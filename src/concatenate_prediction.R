011841388c5672d77cc6c2c8098bb4fe3b40bbcc
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-03-18 10:57:51 -0700

    ♻️ Use lubridate to write file name

--- src/concatenate_prediction.R
+++ src/concatenate_prediction.R
@@ -1,4 +1,5 @@
 library("tidyverse")
+library("lubridate")
 source("/workdir/R/concatenate_prediction.R")
 source("/workdir/R/cli_options.R")
 
@@ -7,9 +8,9 @@ opciones <- get_options_from_names(names_options_cli)
 threshold <- opciones[["threshold"]]
 
 prediction_paths <- obtains_the_last_predictions_of_the_all_leagues()
-  obtain_prediction_of_each_league("39"),
+output_file <- glue::glue("/workdir/results/nies_{today()}.csv")
 predictions <- read_csv(prediction_paths, show_col_types = FALSE)
 cleaned_predictions <- predictions %>%
   filter(home > threshold | away > threshold | draw > threshold) %>%
   arrange(date, league) %>%
-  write_csv("/workdir/results/nies_20-01-2022.csv")
+  write_csv(output_file)
