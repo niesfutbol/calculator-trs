73e42b3303c35af668b046ec9cd070675ea8a3b1
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-03-18 10:57:03 -0700

    ♻️ Use `obtains_the_last_predictions_of_the_all_leagues()`

--- src/concatenate_prediction.R
+++ src/concatenate_prediction.R
@@ -6,16 +6,8 @@ names_options_cli <- c("threshold")
 opciones <- get_options_from_names(names_options_cli)
 threshold <- opciones[["threshold"]]
 
-prediction_paths <- c(
+prediction_paths <- obtains_the_last_predictions_of_the_all_leagues()
   obtain_prediction_of_each_league("39"),
-  obtain_prediction_of_each_league("61"),
-  obtain_prediction_of_each_league("78"),
-  obtain_prediction_of_each_league("88"),
-  obtain_prediction_of_each_league("94"),
-  obtain_prediction_of_each_league("135"),
-  obtain_prediction_of_each_league("140")
-)
-
 predictions <- read_csv(prediction_paths, show_col_types = FALSE)
 cleaned_predictions <- predictions %>%
   filter(home > threshold | away > threshold | draw > threshold) %>%
