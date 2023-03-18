5f9669b2f12720fbd03b10ecc23828faf7fdd9d8
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-03-18 10:36:52 -0700

    ✅ Pass test

--- R/concatenate_prediction.R
+++ R/concatenate_prediction.R
@@ -1,6 +1,16 @@
 library(comprehenr)
 library(tidyverse)
 
+
+obtains_the_last_predictions_of_the_all_leagues <- function(root_path = "/workdir/results") {
+  prediction_paths <- c(
+    obtain_prediction_of_each_league("78", root_path),
+    obtain_prediction_of_each_league("88", root_path),
+    obtain_prediction_of_each_league("94", root_path)
+  )
+  return(prediction_paths)
+}
+
 obtain_the_last_round_file <- function(predictions_files) {
   files <- str_split(predictions_files, "_")
   rounds <- to_vec(for (file in files) file[4])
