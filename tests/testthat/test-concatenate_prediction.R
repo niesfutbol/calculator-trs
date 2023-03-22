2923e93dff0a8c7e16e0faf72013ce27632176cb
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-03-21 19:50:09 -0700

    ✅🪲 Fix bug: sort by number, not by character

--- tests/testthat/test-concatenate_prediction.R
+++ tests/testthat/test-concatenate_prediction.R
@@ -11,7 +11,7 @@ describe("obtain_the_last_round_file", {
     expect_equal(obtained, expected)
   })
   it("returns 34", {
-    files <- c("predictions_78_2022_16.csv", "predictions_78_2022_14.csv", "predictions_78_2022_34.csv")
+    files <- c("predictions_78_2022_16.csv", "predictions_78_2022_7.csv", "predictions_78_2022_34.csv")
     expected <- "predictions_78_2022_34.csv"
     obtained <- obtain_the_last_round_file(files)
     expect_equal(obtained, expected)
